ca2c5b3e81a1497ebe6e400cdbe4748cba40d4f0
Matthew9512 <mateuszmaciak95@gmail.com>
2023-10-27 20:09:45 +0200

    bug

--- frontend/src/pages/Home/Components/FeaturedTravels/FeaturedTravels.jsx
+++ frontend/src/pages/Home/Components/FeaturedTravels/FeaturedTravels.jsx
@@ -13,39 +13,42 @@ export const FeaturedTravels = ({ data, error, loading }) => {
          </h2>
          <article className='destinations'>
             {loading && <LoadingSpinner loading={loading} />}
-            {!data && <p>No travels available at this moment</p>}
-            {data.map((value) => {
-               return (
-                  <div key={value?._id} className='destinations__wrapper'>
-                     <div className='destinations__item'>
-                        <img src={value?.image} className='destinations__img' alt='travel destination image' />
-                        <div className={`unavailable ${value?.availablePlaces <= 0 ? '' : `hidden`}`}>
-                           <p className='unavailable__text'>Temporary unavailable</p>
-                        </div>
-                        <div className='destinations__item-wrapper'>
-                           <p className='destinations-name'>{value?.city}</p>
-                           <p className='destinations-country'>
-                              <FontAwesome iconName='location-dot' /> {value?.country}
-                           </p>
-                           <div className='destinations-date'>
-                              <FontAwesome iconName='plane' />
-                              <p>
-                                 {value?.startDate} - {value?.endDate}
+            {!data ? (
+               <p>No travels available at this moment</p>
+            ) : (
+               data.map((value) => {
+                  return (
+                     <div key={value?._id} className='destinations__wrapper'>
+                        <div className='destinations__item'>
+                           <img src={value?.image} className='destinations__img' alt='travel destination image' />
+                           <div className={`unavailable ${value?.availablePlaces <= 0 ? '' : `hidden`}`}>
+                              <p className='unavailable__text'>Temporary unavailable</p>
+                           </div>
+                           <div className='destinations__item-wrapper'>
+                              <p className='destinations-name'>{value?.city}</p>
+                              <p className='destinations-country'>
+                                 <FontAwesome iconName='location-dot' /> {value?.country}
                               </p>
+                              <div className='destinations-date'>
+                                 <FontAwesome iconName='plane' />
+                                 <p>
+                                    {value?.startDate} - {value?.endDate}
+                                 </p>
+                              </div>
+                              <div className='destinations__item-details'>
+                                 <p className='destinations-category'>{value?.type}</p>
+                                 <p className='destinations-price'>{value?.price}$ / per</p>
+                              </div>
+                              <p className='destinations-description'>{value?.description}</p>
+                              <Link to={`/search/${value?._id}`} className='btn btn-more'>
+                                 See more
+                              </Link>
                            </div>
-                           <div className='destinations__item-details'>
-                              <p className='destinations-category'>{value?.type}</p>
-                              <p className='destinations-price'>{value?.price}$ / per</p>
-                           </div>
-                           <p className='destinations-description'>{value?.description}</p>
-                           <Link to={`/search/${value?._id}`} className='btn btn-more'>
-                              See more
-                           </Link>
                         </div>
                      </div>
-                  </div>
-               );
-            })}
+                  );
+               })
+            )}
          </article>
       </>
    );
